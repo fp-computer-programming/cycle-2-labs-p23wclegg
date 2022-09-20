08210695db4ef282105378d339a3585d4526b1dd
William <p23wclegg@fairfieldprep.org>
2022-09-20 13:43:52 -0400

    these are my labs

---
+++ lab-2-6.py
@@ -0,0 +1,25 @@
+#Lab 2-6
+from xmlrpc.client import Boolean
+
+
+a=5
+b=10
+c=0
+d=-4
+
+if a%2==0:
+    print('a is an even number')
+else:
+    print('a is an odd number')
+if b%2==0:
+    print('b is an even number')
+else:
+    print('b is an odd number')
+if c%2==0:
+    print('c is an even number')
+else:
+    print('c is an odd number')
+if d%2==0:
+    print('d is an even number')
+else:
+    print('d is an odd number')
---
+++ lab-2-7.py
@@ -0,0 +1,54 @@
+#Lab 2-6
+from xmlrpc.client import Boolean
+
+
+a=5
+b=10
+c=0
+d=-4
+
+if a%2==0:
+    print('a is an even number')
+else:
+    print('a is an odd number')
+if b%2==0:
+    print('b is an even number')
+else:
+    print('b is an odd number')
+if c%2==0:
+    print('c is an even number')
+else:
+    print('c is an odd number')
+if d%2==0:
+    print('d is an even number')
+else:
+    print('d is an odd number')
+
+
+if a<-5:
+    print ('a is less than -5')
+elif a>5:
+    print ('a is greater than 5')
+else:
+    print ('a is between -5 and 5')
+
+if b<-5:
+    print ('a is less than -5')
+elif b>5:
+    print ('a is greater than 5')
+else:
+    print ('a is between -5 and 5')
+
+if c<-5:
+    print ('a is less than -5')
+elif c>5:
+    print ('a is greater than 5')
+else:
+    print ('a is between -5 and 5')
+
+if d<-5:
+    print ('a is less than -5')
+elif d>5:
+    print ('a is greater than 5')
+else:
+    print ('a is between -5 and 5')
