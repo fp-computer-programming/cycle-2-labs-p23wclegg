e8ee6365df7739529cab14f5c95da4689328cab4
William <p23wclegg@fairfieldprep.org>
2022-09-21 14:29:51 -0400

    asdkf;alsdfj;a

---
+++ lab-2-8.py
@@ -0,0 +1,10 @@
+Team_A=10
+if Team_A<9:
+    print('no medal')
+else: 
+    if Team_A<12:
+        print('third')
+    else: 
+        if Team_A<15: 
+                print('second place') 
+        else: print('First Place')
